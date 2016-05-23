46969b15ce3a950d3c3ae7c53ae9258f72846fe1
pecopeland <pecopeland@ebay.com>
2016-05-23 00:05:13 -0700

    Getting lasso to work

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ require('marko/node-require').install();
 
 var express = require('express');
 var app = express();
-app.set('config', 'config');
 app.set('port', (process.env.PORT || 5000));
 
 var config = require('./config/config.json');
 require('lasso').configure(config.lasso);
+app.use(require('lasso/middleware').serveStatic());
 
 app.get('/', require('./src/pages/index'));
 
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,5 +1,6 @@
 var template = require('./index.marko');
 
 module.exports = function(req, res) {
+  res.set('Content-Type', 'text/html; charset=utf-8');
   template.render({}, res);
 }
