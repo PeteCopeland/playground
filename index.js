a5d818471803d106f0d9eae795ee28582df04ccb
pecopeland <pecopeland@ebay.com>
2016-05-22 20:55:14 -0700

    Fixes for heroku

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function(req, res) {
   res.send('Hello world!');
 });
 
-app.listen(8080, function() {
-  console.log('App started on 8080');
+app.listen(app.get('port'), function() {
+  console.log('App started on ', app.get('port'));
 });
