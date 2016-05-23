e71358f04e9c70c306c06fc987b6a91901b03d67
pecopeland <pecopeland@ebay.com>
2016-05-22 21:42:49 -0700

    Adding marko and lasso support

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+require('marko/node-require').install();
+
 var express = require('express');
 var app = express();
-
+app.set('config', 'config');
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(req, res) {
-  res.send('Hello world!');
-});
+var config = require('./config/config.json');
+require('lasso').configure(config.lasso);
+
+app.get('/', require('./src/pages/index'));
 
 app.listen(app.get('port'), function() {
   console.log('App started on ', app.get('port'));
